ec840a9dd4daf1699b8079326b678274fbf059dc
okondev2021 <sojapril16@gmail.com>
2024-01-17 00:19:06 +0100

    solved 2

--- longestPrefix.py
+++ longestPrefix.py
@@ -1,4 +1,6 @@
 def longest_common_prefix(strs: list):
+    if len(strs) < 1:
+        return ""
 
     strs.sort(key=lambda x: len(x))
     shortest_char = strs[0]
@@ -6,18 +8,22 @@ def longest_common_prefix(strs: list):
     stack = list()
 
     while index_position < len(shortest_char):
+
         string_stack = str()
+
         for char in strs:
             string_stack += char[index_position]
+
         if len(set(string_stack)) == 1:
             stack.append(string_stack[0])
-            index_position = index_position + 1
         else:
             if len(stack) > 0:
                 return "".join(stack)
             else:
                 return ""
+        index_position = index_position + 1
+    return "".join(stack)
 
 
-print(longest_common_prefix(["low", "loght"]))
+print(longest_common_prefix(['at']))
 
